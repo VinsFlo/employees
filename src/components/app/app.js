3278285e94de7f3b079b869c35b90976e41e9427
VinsFlo <vinsflorineus@gmail.com>
2021-12-03 18:16:37 +0300

    feat: filter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -18,6 +18,7 @@ class App extends Component {
 				{ name: 'Carl W.', salary: 5000, increase: false, rise: false, id: 3 }
 			],
 			term: '',
+			filter: 'all'
 		}
 		this.maxId = 4;
 	}
@@ -29,7 +30,6 @@ class App extends Component {
 			}
 		})
 	}
-
 	addItem = (name, salary) => {
 		const newItem = {
 			name,
@@ -71,11 +71,26 @@ class App extends Component {
 		this.setState({ term });
 	}
 
+	filterPost = (items, filter) => {
+		switch (filter) {
+			case 'rise':
+				return items.filter(item => item.rise);
+			case 'moreThen1000':
+				return items.filter(item => item.salary > 1000);
+			default:
+				return items
+		}
+	}
+
+	onFilterSelect = (filter) => {
+		this.setState({ filter });
+	}
+
 	render() {
 		const { data, term, filter } = this.state;
 		const employees = this.state.data.length;
 		const increased = this.state.data.filter(item => item.increase).length;
-		const visibleData = this.searchEmp(data, term);
+		const visibleData = this.filterPost(this.searchEmp(data, term), filter);
 
 		return (
 			<div className="app">
